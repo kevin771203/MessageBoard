5426fcb824c5789decc4edb15850a310a55d93c9
Kevin <b0952193319@gmail.com>
2025-01-16 11:24:19 +0800

    add_message_test

---
+++ src/main/java/org/example/MessageBoard.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MessageBoard {
+
+
+
+    public String addMessage(int id, String name, String title, String text) {
+
+        return "add message success!!";
+    }
+
+
+}
---
+++ src/test/java/org/example/MessageBoardTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+class MessageBoardTest {
+
+    @Test
+    void add_message() {
+
+        MessageBoard messageBoard = new MessageBoard();
+        String result= messageBoard.addMessage(1,"我的名字", "我的標題", "我的內容");
+        Assertions.assertThat(result).isEqualTo("add message success!!");
+
+
+    }
+
+
+}
